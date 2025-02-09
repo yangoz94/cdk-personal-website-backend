1d8e80230a3de496562fd37ce0950e37a0cc3853
Oğuzhan Yangöz <oguzhan.yangoz@fulbrightmail.org>
2025-02-09 01:05:13 -0500

    add tsconfig to lambda

--- src/lambdas/hello/hello.ts
+++ src/lambdas/hello/hello.ts
@@ -10,7 +10,7 @@ const requestBodySchema = z.object({
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  console.log(`Function invoked with the following: ${JSON.stringify(event)}`);
+  console.log(`Function innvoked with the following: ${JSON.stringify(event)}`);
 
   let body;
   try {
